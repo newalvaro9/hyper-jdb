9c39567eddb5153dc233fed59a787c0b4a3a3e6e
newalvaro9 <alvaropobladoresteban9@gmail.com>
2023-02-26 21:37:45 +0100

    constructor db creation

--- index.ts
+++ index.ts
@@ -6,6 +6,16 @@ import { writeFile, writeFileSync } from "fs"
 
 
 class ultraJDB {
+    path_folder: string
+    path_database: string
+
+    constructor(public name: string) {
+        if (!name) throw new ErrorUJDB("Invalid Value", "You must provide a name to your database")
+        const data = create_database(name)
+        this.name = name;
+        this.path_folder = data.path_folder;
+        this.path_database = data.path_database
+    }
 }
 
 export = ultraJDB
