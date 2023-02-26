e2e0cb5f6fb8b93a5c50929fc254bded7213c0b9
newalvaro9 <alvaropobladoresteban9@gmail.com>
2023-02-26 21:37:12 +0100

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,11 @@
+// @ts-check
+
+import ErrorUJDB from "./Errors/ErrorUJDB"
+import create_database from "./Utils/create_database"
+import { writeFile, writeFileSync } from "fs"
+
+
+class ultraJDB {
+}
+
+export = ultraJDB
