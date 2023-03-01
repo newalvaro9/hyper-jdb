2ba0805b9209ef66651ab925e8479436b2d0e2d9
newalvaro9 <alvaropobladoresteban9@gmail.com>
2023-03-01 16:37:50 +0100

    created read_file function

---
+++ Utils/read_file.ts
@@ -0,0 +1,11 @@
+import { readFileSync } from "fs";
+import ErrorUJDB from "../Errors/ErrorUJDB";
+
+export default function read_file(db_path: string) {
+    try {
+        const reading = readFileSync(db_path, 'utf-8')
+        return JSON.parse(reading)
+    } catch (error) {
+        throw new ErrorUJDB("Unknow Directory", "No such file or directory when reading file")
+    }
+}
